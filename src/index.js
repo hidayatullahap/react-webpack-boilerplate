dab6e6d0fc51d36480f875a337ef674631777122
Hidayatullah Agung Prasetyo <hidayatullahap@gmail.com>
2019-04-21 15:44:17 +0700

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../style/style.css';
 
 class App extends Component {
@@ -17,50 +19,59 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Tester</h1>
-				<p>{this.state.changer + ''}</p>
-				<button className="btn"
-					onClick={() => this.setState({ changer: !this.state.changer })}>
-					State Change
-				</button>
-				<div className="row">
-					<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-						<button className="btn"
-							onClick={() => {
-								document.body.style.backgroundColor = '#ed6a5a';
-							}}>
-							ed6a5a
-						</button>
-					</div>
-					<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-						<button className="btn"
-							onClick={() => {
-								document.body.style.backgroundColor = '#fbfffe';
-							}}>
-							fbfffe
-						</button>
-					</div>
-					<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-						<button className="btn"
-							onClick={() => {
-								document.body.style.backgroundColor = '#f6a90a';
-							}}>
-							f6a90a
-						</button>
-					</div>
-					<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-						<button className="btn"
-							onClick={() => {
-								document.body.style.backgroundColor = '#9bc1bc';
-							}}>
-							9bc1bc
-						</button>
+				<div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+					<h1>Tester</h1>
+					<p>{this.state.changer + ''}</p>
+					<button className="btn"
+						onClick={() => this.setState({ changer: !this.state.changer })}>
+						State Change
+					</button>
+					<div className="row">
+						<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+							<button className="btn"
+								onClick={() => {
+									document.body.style.backgroundColor = '#ed6a5a';
+								}}>
+								ed6a5a
+							</button>
+						</div>
+						<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+							<button className="btn"
+								onClick={() => {
+									document.body.style.backgroundColor = '#fbfffe';
+								}}>
+								fbfffe
+							</button>
+						</div>
+						<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+							<button className="btn"
+								onClick={() => {
+									document.body.style.backgroundColor = '#f6a90a';
+								}}>
+								f6a90a
+							</button>
+						</div>
+						<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+							<button className="btn"
+								onClick={() => {
+									document.body.style.backgroundColor = '#9bc1bc';
+								}}>
+								9bc1bc
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
 		);
 	}
 }
+
+
+WebFont.load({
+	google: {
+		families: ['Roboto:300,400,700']
+	}
+});
 
 ReactDOM.render(
 	<App />,
